eb877e3a8ec2cbf5d168a459f288fc9f8b93564d
Tanay <tanayj2107@gmail.com>
2025-11-12 11:49:41 -0500

    returned a list of all names

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #TODO: Create a letter using starting_letter.txt 
+
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
@@ -6,3 +7,6 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+f = open("starting_letter.txt", "r")
+print(f.readlines())
